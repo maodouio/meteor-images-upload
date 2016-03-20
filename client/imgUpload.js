41366ed0468e5619ff7247cdce80ad73f47ba64e
dououFullstack <13412133311@139.com>
2016-03-20 15:53:03 +0800

    uplode []

--- client/imgUpload.js
+++ client/imgUpload.js
@@ -3,23 +3,22 @@ Meteor.call("getQiniuDomain", function(error, result) {
     Session.set("qiniuUploadDomain", result);
   }
 });
-Template.imgUploader.onRendered = function() {
-  Session.set("logoImage", "");
-  // Session.set("imageFileName", "false");
 
-}
+Template.imgUploader.onRendered(function() {
+  Session.set("logoImage", []);
+  console.log(Session.set("logoImage", []));
+  Session.set("imageFileName", "false");
+});
 
 Template.imgUploader.events({
-  'click #myFileInput': function() {
     // $('#myFileInput').trigger("click");
-  },
 
   'change #myFileInput': function(e, template) {
     // 读取所有files
     var files = event.target.files;
     if (files.length === 0) {
       Session.set("imageFileName", "未选择文件");
-      Session.set("logoImage", "");
+      Session.set("logoImage", []);
       return;
     }
     // WARN: 选取file的第一个，以后需要循环所有图片
@@ -38,11 +37,11 @@ Template.imgUploader.events({
       Meteor.call('sendAvatarInBase64', dataUrl, function(err, res) {
         if (res) {
           // document.querySelector('img').src = Session.get("qiniuUploadDomain") + res;
-          var imgUrl = Session.get("qiniuUploadDomain") + res + "?imageView2/2/w/200/h/55";
+          var imgArr = Session.get("logoImage");
+          console.log(typeof(imgArr));
+          imgArr.push(Session.get("qiniuUploadDomain") + res + "?imageView2/2/w/200/h/55");
 
-          // $("#imageReveal").attr("src", imgUrl);
-          // $("#imageReveal").css("display", "inline-block");
-          Session.set("logoImage", imgUrl);
+          Session.set("logoImage", imgArr);
           // console.log(Session.get("qiniuUploadDomain") + res);
           alert('图片上传成功');
         } else {
