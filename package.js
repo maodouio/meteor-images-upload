18d0d584b61386a6cebc541e7a75592b705d5136
limingth <limingth@gmail.com>
2016-03-22 10:29:50 +0800

    Change name to images

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'maodouio:meteor-image-upload',
+  name: 'maodouio:meteor-images-upload',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: '',
