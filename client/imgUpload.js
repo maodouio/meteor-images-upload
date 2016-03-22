e738a5fe38f42e8f4ce1230b467262eabe7e4ab3
limingth <limingth@gmail.com>
2016-03-22 10:27:40 +0800

    Change image to images

--- client/imgUpload.js
+++ client/imgUpload.js
@@ -5,8 +5,8 @@ Meteor.call("getQiniuDomain", function(error, result) {
 });
 
 Template.imgUploader.onRendered(function() {
-  Session.set("logoImage", []);
-  console.log(Session.set("logoImage", []));
+  Session.set("logoImages", []);
+  //console.log(Session.set("logoImages", []));
   Session.set("imageFileName", "false");
 });
 
@@ -18,7 +18,7 @@ Template.imgUploader.events({
     var files = event.target.files;
     if (files.length === 0) {
       Session.set("imageFileName", "未选择文件");
-      Session.set("logoImage", []);
+      Session.set("logoImages", []);
       return;
     }
     // WARN: 选取file的第一个，以后需要循环所有图片
@@ -37,11 +37,11 @@ Template.imgUploader.events({
       Meteor.call('sendAvatarInBase64', dataUrl, function(err, res) {
         if (res) {
           // document.querySelector('img').src = Session.get("qiniuUploadDomain") + res;
-          var imgArr = Session.get("logoImage");
+          var imgArr = Session.get("logoImages");
           console.log(typeof(imgArr));
           imgArr.push(Session.get("qiniuUploadDomain") + res + "?imageView2/2/w/200/h/55");
 
-          Session.set("logoImage", imgArr);
+          Session.set("logoImages", imgArr);
           // console.log(Session.get("qiniuUploadDomain") + res);
           alert('图片上传成功');
         } else {
